7359e2a7f49ae9d961405ebf5d8d5cb72f6f45ea
Arthur Machado <44805300+arthurrmp@users.noreply.github.com>
2026-03-26 09:39:53 -0300

    Update portcrush formula sha256

--- Formula/portcrush.rb
+++ Formula/portcrush.rb
@@ -2,7 +2,7 @@ class Portcrush < Formula
   desc "Beautiful TUI port manager"
   homepage "https://github.com/arthurrmp/portcrush"
   url "https://github.com/arthurrmp/portcrush/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "b8f17beaf66b4196981e6379f2ce826aea0bfd8e3a2986acbc4e727c11e41b1f"
+  sha256 "9a0713800f9150e68fc8f69743215fb70ee4187c694d40c3b0a3d1029feadc17"
   license "MIT"
 
   depends_on "rust" => :build
