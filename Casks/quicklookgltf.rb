2e0ee151e84c12439e0c81b14b3fa4d4bc2d19de
Arthur Machado <44805300+arthurrmp@users.noreply.github.com>
2026-04-06 12:22:28 -0300

    Update quicklookgltf to v1.2.0

--- Casks/quicklookgltf.rb
+++ Casks/quicklookgltf.rb
@@ -1,6 +1,6 @@
 cask "quicklookgltf" do
-  version "1.1.0"
-  sha256 "c022f01236b52182a6b06ca6c949fbb1dadbca144bbe73c62713d9b1b25359d8"
+  version "1.2.0"
+  sha256 "6199ace95878513175d43d16f3cbc1d5b00efe49b0c2ff0ade06bffba9b31bd5"
 
   url "https://github.com/arthurrmp/QuickLookGLTF/releases/download/v#{version}/QuickLookGLTF.zip"
   name "QuickLookGLTF"
